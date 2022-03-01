9fbe54ae2a83b0edabda5e6413901345cc84172d
Angelo <angelojorssatto@gmail.com>
2022-02-28 22:56:15 -0300

    graficos finalizados e ajuste no tempo

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from MergeSort import mergeSort
 from QuickSort import quickSort
 from HeapSort import heapSort
 from Graphs import creatGraphs
-import time 
+import time
 import copy
 import pandas as pd
 import sys
@@ -14,22 +14,22 @@ sys.setrecursionlimit(1500)
 results = []
 
 # Lista todos os arquivos .txt 
-#files = glob.glob(r"D:\Documentos\CIENCIA DA COMPUTAÇAO\4º ANO\Projeto e Análise de Algoritmos\Trabalho 1\**\*.txt", recursive=True)
+files = glob.glob(r"D:\Documentos\CIENCIA DA COMPUTAÇAO\4º ANO\Projeto e Análise de Algoritmos\Trabalho 1\**\*.txt", recursive=True)
 
-files = [
-    r"D:\Documentos\CIENCIA DA COMPUTAÇAO\4º ANO\Projeto e Análise de Algoritmos\Trabalho 1\Aleatórios\a1000.txt",
-    r"D:\Documentos\CIENCIA DA COMPUTAÇAO\4º ANO\Projeto e Análise de Algoritmos\Trabalho 1\Aleatórios\a100.txt",
-    r"D:\Documentos\CIENCIA DA COMPUTAÇAO\4º ANO\Projeto e Análise de Algoritmos\Trabalho 1\Aleatórios\a500.txt",
-    r"D:\Documentos\CIENCIA DA COMPUTAÇAO\4º ANO\Projeto e Análise de Algoritmos\Trabalho 1\Decrescentes\d1000.txt",
-    r"D:\Documentos\CIENCIA DA COMPUTAÇAO\4º ANO\Projeto e Análise de Algoritmos\Trabalho 1\Decrescentes\d100.txt",
-    r"D:\Documentos\CIENCIA DA COMPUTAÇAO\4º ANO\Projeto e Análise de Algoritmos\Trabalho 1\Decrescentes\d500.txt",
-    r"D:\Documentos\CIENCIA DA COMPUTAÇAO\4º ANO\Projeto e Análise de Algoritmos\Trabalho 1\Ordenados\o1000.txt",
-    r"D:\Documentos\CIENCIA DA COMPUTAÇAO\4º ANO\Projeto e Análise de Algoritmos\Trabalho 1\Ordenados\o100.txt",    
-    r"D:\Documentos\CIENCIA DA COMPUTAÇAO\4º ANO\Projeto e Análise de Algoritmos\Trabalho 1\Ordenados\o500.txt",
-    r"D:\Documentos\CIENCIA DA COMPUTAÇAO\4º ANO\Projeto e Análise de Algoritmos\Trabalho 1\ParcialmenteOrdenados\po1000.txt",
-    r"D:\Documentos\CIENCIA DA COMPUTAÇAO\4º ANO\Projeto e Análise de Algoritmos\Trabalho 1\ParcialmenteOrdenados\po100.txt",
-    r"D:\Documentos\CIENCIA DA COMPUTAÇAO\4º ANO\Projeto e Análise de Algoritmos\Trabalho 1\ParcialmenteOrdenados\po500.txt",
-]
+# files = [
+#     r"D:\Documentos\CIENCIA DA COMPUTAÇAO\4º ANO\Projeto e Análise de Algoritmos\Trabalho 1\Aleatórios\a1000.txt",
+#     r"D:\Documentos\CIENCIA DA COMPUTAÇAO\4º ANO\Projeto e Análise de Algoritmos\Trabalho 1\Aleatórios\a100.txt",
+#     r"D:\Documentos\CIENCIA DA COMPUTAÇAO\4º ANO\Projeto e Análise de Algoritmos\Trabalho 1\Aleatórios\a500.txt",
+#     r"D:\Documentos\CIENCIA DA COMPUTAÇAO\4º ANO\Projeto e Análise de Algoritmos\Trabalho 1\Decrescentes\d1000.txt",
+#     r"D:\Documentos\CIENCIA DA COMPUTAÇAO\4º ANO\Projeto e Análise de Algoritmos\Trabalho 1\Decrescentes\d100.txt",
+#     r"D:\Documentos\CIENCIA DA COMPUTAÇAO\4º ANO\Projeto e Análise de Algoritmos\Trabalho 1\Decrescentes\d500.txt",
+#     r"D:\Documentos\CIENCIA DA COMPUTAÇAO\4º ANO\Projeto e Análise de Algoritmos\Trabalho 1\Ordenados\o1000.txt",
+#     r"D:\Documentos\CIENCIA DA COMPUTAÇAO\4º ANO\Projeto e Análise de Algoritmos\Trabalho 1\Ordenados\o100.txt",    
+#     r"D:\Documentos\CIENCIA DA COMPUTAÇAO\4º ANO\Projeto e Análise de Algoritmos\Trabalho 1\Ordenados\o500.txt",
+#     r"D:\Documentos\CIENCIA DA COMPUTAÇAO\4º ANO\Projeto e Análise de Algoritmos\Trabalho 1\ParcialmenteOrdenados\po1000.txt",
+#     r"D:\Documentos\CIENCIA DA COMPUTAÇAO\4º ANO\Projeto e Análise de Algoritmos\Trabalho 1\ParcialmenteOrdenados\po100.txt",
+#     r"D:\Documentos\CIENCIA DA COMPUTAÇAO\4º ANO\Projeto e Análise de Algoritmos\Trabalho 1\ParcialmenteOrdenados\po500.txt",
+# ]
 
 for file in files:
     print(file)
@@ -53,27 +53,27 @@ for file in files:
 
     # Merge
     v_tosort = copy.deepcopy(v)
-    begin = time.time_ns()
+    begin = time.perf_counter_ns()
     mergeSort(v_tosort, 0, len(v_tosort)-1)
-    end = time.time_ns()
+    end = time.perf_counter_ns()
     exe_time = end-begin
     print("tempo execução merge (ns)", exe_time, begin, end)
     results.append(['Merge', mode, len(v_tosort), begin, end, exe_time])
 
     # Quick
     v_tosort = copy.deepcopy(v)
-    begin = time.time_ns()
+    begin = time.perf_counter_ns()
     quickSort(v_tosort, 0, len(v_tosort)-1)
-    end = time.time_ns()
+    end = time.perf_counter_ns()
     exe_time = end-begin
     print("tempo execução quick (ns)", exe_time, begin, end)
     results.append(['Quick', mode, len(v_tosort), begin, end, exe_time])
     
     # Heap
     v_tosort = copy.deepcopy(v)
-    begin = time.time_ns()
+    begin = time.perf_counter_ns()
     heapSort(v_tosort)
-    end = time.time_ns()
+    end = time.perf_counter_ns()
     exe_time = end-begin
     print("tempo execução heap (ns)", exe_time, begin, end) 
     results.append(['Heap', mode, len(v_tosort), begin, end, exe_time])
